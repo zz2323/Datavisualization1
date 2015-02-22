ab741bde984ee4bb7cb17519dbca6805df3aa774
ZhihaoZhang <zz2323@columbia.edu>
2015-02-22 12:12:06 -0500

    java script for function.

---
+++ scripts.js
@@ -0,0 +1,53 @@
+
+//credit Google
+//Load the Google visualization API and the scatter chart package
+google.load("visualization", "1", {
+				packages : ["corechart"]
+			});
+			google.setOnLoadCallback(drawChart);
+			function drawChart() {
+
+				var myPopularSeries = [];//an empty array to put all the data
+
+				var myObs = AllMyData.tags;//changed the name tag to follow the D.R.Y principle
+				
+				/*
+				 * A for loop to get the name and popularity from AllMyData
+				 * 
+				 */
+				for (var i = 0; i < myObs.length; i++) {
+
+					var namesAndPopulartiy = [];
+					namesAndPopulartiy.push(myObs[i].name);
+					namesAndPopulartiy.push(myObs[i].popularity);//the string is already in digit, so I don't need the Number function to convert.
+					myPopularSeries.push(namesAndPopulartiy);
+				}
+
+				console.log(myPopularSeries);
+
+				var data = new google.visualization.DataTable();
+				data.addColumn('string', 'Name');//add the hAxis
+				data.addColumn('number', 'Popularity');//add the vAxis
+				data.addRows(myPopularSeries);
+
+				// Set chart options
+				var options = {
+					
+					title : 'How Popular are these Series Search',
+					hAxis : {
+						title : 'Popularity',
+						minValue : 0,
+						maxValue : 100,//setting the MaxValue to 100
+					},
+					vAxis : {
+						title : 'Series'
+					},
+					legend : 'top' //putting the legend on top
+				};
+				/*
+				 * changed scatter chart into a bar chart
+				 */
+
+				var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
+				chart.draw(data, options);
+			}
