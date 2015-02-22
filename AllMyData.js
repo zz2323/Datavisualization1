1e66a074d2eb792b4b8a83edf9d649c17c03c573
ZhihaoZhang <zz2323@columbia.edu>
2015-02-22 12:11:53 -0500

    java script for all my data.

---
+++ AllMyData.js
@@ -0,0 +1,136 @@
+			var AllMyData = {
+				"realtime_start" : "2013-08-14",
+				"realtime_end" : "2013-08-14",
+				"order_by" : "series_count",
+				"sort_order" : "desc",
+				"count" : 18,
+				"offset" : 0,
+				"limit" : 1000,
+				"tags" : [{
+					"name" : "academic data",
+					"group_id" : "gen",
+					"notes" : "Time series data created mainly by academia to address growing demand in understanding specific concerns in the economy that are not well modeled by ordinary statistical agencies.",
+					"created" : "2012-08-29 10:22:19-05",
+					"popularity" : 62,
+					"series_count" : 25
+				}, {
+					"name" : "anderson & jones",
+					"group_id" : "src",
+					"notes" : "Richard Anderson and Barry Jones",
+					"created" : "2013-06-21 10:22:49-05",
+					"popularity" : 46,
+					"series_count" : 25
+				}, {
+					"name" : "divisia",
+					"group_id" : "gen",
+					"notes" : "Monetary Services Indexes",
+					"created" : "2012-02-27 10:18:19-06",
+					"popularity" : 46,
+					"series_count" : 25
+				}, {
+					"name" : "frb stl",
+					"group_id" : "src",
+					"notes" : "Federal Reserve Bank of St. Louis (source)",
+					"created" : "2012-02-27 10:18:19-06",
+					"popularity" : 81,
+					"series_count" : 25
+				}, {
+					"name" : "monetary aggre.",
+					"group_id" : "gen",
+					"notes" : "",
+					"created" : "2012-02-27 10:18:19-06",
+					"popularity" : 79,
+					"series_count" : 25
+				}, {
+					"name" : "monthly",
+					"group_id" : "freq",
+					"notes" : "",
+					"created" : "2012-02-27 10:18:19-06",
+					"popularity" : 95,
+					"series_count" : 25
+				}, {
+					"name" : "nation",
+					"group_id" : "geot",
+					"notes" : "Country Level",
+					"created" : "2012-02-27 10:18:19-06",
+					"popularity" : 100,
+					"series_count" : 25
+				}, {
+					"name" : "usa",
+					"group_id" : "geo",
+					"notes" : "United States of America",
+					"created" : "2012-02-27 10:18:19-06",
+					"popularity" : 100,
+					"series_count" : 25
+				}, {
+					"name" : "sa",
+					"group_id" : "seas",
+					"notes" : "Seasonally adjusted",
+					"created" : "2012-02-27 10:18:19-06",
+					"popularity" : 94,
+					"series_count" : 13
+				}, {
+					"name" : "indexes",
+					"group_id" : "gen",
+					"notes" : "",
+					"created" : "2012-02-27 10:18:19-06",
+					"popularity" : 79,
+					"series_count" : 12
+				}, {
+					"name" : "nsa",
+					"group_id" : "seas",
+					"notes" : "Not seasonally adjusted",
+					"created" : "2012-02-27 10:18:19-06",
+					"popularity" : 96,
+					"series_count" : 12
+				}, {
+					"name" : "real",
+					"group_id" : "gen",
+					"notes" : "Inflation Adjusted Data",
+					"created" : "2012-02-27 10:18:19-06",
+					"popularity" : 81,
+					"series_count" : 12
+				}, {
+					"name" : "assets",
+					"group_id" : "gen",
+					"notes" : "",
+					"created" : "2012-02-27 10:18:19-06",
+					"popularity" : 69,
+					"series_count" : 4
+				}, {
+					"name" : "m2",
+					"group_id" : "gen",
+					"notes" : "M2 Money Stock",
+					"created" : "2012-02-27 10:18:19-06",
+					"popularity" : 75,
+					"series_count" : 3
+				}, {
+					"name" : "m1",
+					"group_id" : "gen",
+					"notes" : "M1 Money Stock",
+					"created" : "2012-02-27 10:18:19-06",
+					"popularity" : 71,
+					"series_count" : 2
+				}, {
+					"name" : "m3",
+					"group_id" : "gen",
+					"notes" : "M3 Money Stock",
+					"created" : "2012-02-27 10:18:19-06",
+					"popularity" : 50,
+					"series_count" : 2
+				}, {
+					"name" : "mzm",
+					"group_id" : "gen",
+					"notes" : "MZM Money Stock",
+					"created" : "2012-02-27 10:18:19-06",
+					"popularity" : 61,
+					"series_count" : 2
+				}, {
+					"name" : "discontinued",
+					"group_id" : "gen",
+					"notes" : "",
+					"created" : "2012-02-27 10:18:19-06",
+					"popularity" : 68,
+					"series_count" : 1
+				}]
+			}
